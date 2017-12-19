ad1437b6d1275550d7847b496471b77cc5b16a14
AntoineYnov <antoine.genouinduhamel@ynov.com>
2017-12-19 15:48:01 +0100

    exercices

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,11 +1,11 @@
 exports.run = function(input) {
   console.log("Exercise #1");
   var total=0;
+  var newTableau = [];
 
 	while (total<3) {
 		console.log(input[total]);
-		var newTableau = [];
-		newTableau = newTableau.push(input[total]);
+		newTableau.push(input[total]);
 		total= total + 1;
 	}
 	return newTableau;
